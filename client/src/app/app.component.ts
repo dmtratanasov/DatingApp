7ad375b051b6cd59700600837f89e24bdbfc645b
Dimitar Atanasov <dimitarata1@hotmail.com>
2021-03-15 23:20:43 +0100

    section 2

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  title = 'Dating App';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getUsers();
+    console.log(this.users);
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users').subscribe(
+      (response) => {
+        this.users = response;
+        console.log(this.users);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+}
